e7dc27a59ee859eb3e8683ab77dd405f63f3b956
Bezmen Egor <bezmen_egor@mail.ru>
2022-06-28 18:35:15 +0300

    added user policy

--- app/graphql/mutations/destroy_record.rb
+++ app/graphql/mutations/destroy_record.rb
@@ -10,7 +10,7 @@ module Mutations
       record = SurveysSchema.object_from_id(record_id, nil)
 
       clazz = record.class.to_s + 'Policy'
-      return unless clazz == 'UserPolicy' or guard_by_policy('update?', policy_class: clazz )
+      return unless guard_by_policy('update?', policy_class: clazz )
 
       record.destroy
     end
